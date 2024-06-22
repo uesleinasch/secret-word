958b9f7d336d5da15a0df13716213e05c175b0fd
uesleidcn <ueslei@ztrax.com.br>
2024-06-22 14:55:02 -0300

    feat(#1) screen movement

--- src/components/levels/EndGame/EndGame.tsx
+++ src/components/levels/EndGame/EndGame.tsx
@@ -2,10 +2,14 @@ import { Dispatch, SetStateAction } from "react"
 import Button from "../../Layouts/buttons/Buttons"
 
 const EndGame = ({setGaeStage}: {setGaeStage: Dispatch<SetStateAction<number>>}) => {
+    const goHome = () => {
+        setGaeStage(2)
+    }
     return (
         <>
             <h1>End Game</h1>
-            <Button action={() => setGaeStage(1) } text="Start Game" type="primary" />
+            <Button action={goHome } text="Start Game" type="primary" />
+            <Button action={() => setGaeStage(1)} text="Home" type="secondary" />
         </>
     )
 }
